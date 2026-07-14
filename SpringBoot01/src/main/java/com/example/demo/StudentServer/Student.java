6962605a38582b48e950cb1aba9d151857c95de5
mansvi <itsmansvi@gmail.com>
2026-07-14 13:41:37 +0530

    GET /student/create

--- SpringBoot01/src/main/java/com/example/demo/StudentServer/Student.java
+++ SpringBoot01/src/main/java/com/example/demo/StudentServer/Student.java
@@ -10,4 +10,46 @@ public class Student {
     String name;
     int age;
     String department;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                '}';
+    }
 }
--- SpringBoot01/src/main/java/com/example/demo/StudentServer/StudentController.java
+++ SpringBoot01/src/main/java/com/example/demo/StudentServer/StudentController.java
@@ -1,8 +1,19 @@
 package com.example.demo.StudentServer;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
+    // Store the student
+    @GetMapping("/create")
+    public String storeStudent() { return "id: 1, name: abc"; }
 
+    // Read the Student by ID
+
+    // Update the student information
+
+    // Delete the student information
 }
