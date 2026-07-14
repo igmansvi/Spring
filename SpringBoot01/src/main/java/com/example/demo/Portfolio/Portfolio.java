20cacfe663f140eee82fef1845cc41c8453eb22b
mansvi <itsmansvi@gmail.com>
2026-07-14 11:13:59 +0530

    /skills

--- SpringBoot01/src/main/java/com/example/demo/Portfolio/Portfolio.java
+++ SpringBoot01/src/main/java/com/example/demo/Portfolio/Portfolio.java
@@ -11,15 +11,23 @@ public class Portfolio {
                    <div style="text-align: center; border: 1px solid black; width: 50%; background-color: #FF00FF; color: black;">
                         <h1> Mansvi Kumar </h1>
                            <p> I am from Patna, Bihar. Currently pursuing B.Tech CSE from Lovely Professional University </p>
-                           <h3> Tech Stack <h3>
-                           <ol>
-                                <li>Java</li>
-                                <li>Spring</li>
-                           </ol>
                            <ul>
                                 <li>Github Link: <a href='https://github.com/igmansvi/Spring' target='_blank'>github.com/igmansvi/Spring</a></li>
                            </ul>
                    </div>
+                """;
+    }
+
+    @GetMapping("/skills")
+    public String skills() {
+        return """
+                    <div style="text-align: center; border: 1px solid black; width: 50%; background-color: #FF00FF; color: black;">
+                        <h1> Tech Stack </h1>
+                        <ul>
+                            <li>Java</li>
+                            <li>Spring</li>
+                        </ul>
+                    </div>
                 """;
     }
 }
