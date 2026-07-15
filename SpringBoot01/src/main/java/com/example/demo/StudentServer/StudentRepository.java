aa66fa944560dae2f21413fd5912b7494f171f6d
mansvi <itsmansvi@gmail.com>
2026-07-15 11:30:30 +0530

    Spring Data JPA

--- SpringBoot01/src/main/java/com/example/demo/StudentServer/StudentRepository.java
+++ SpringBoot01/src/main/java/com/example/demo/StudentServer/StudentRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.StudentServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
-    public Student save(Student student) {
-        System.out.println("Student created successfully!");
-        return student;
-    }
-}
+public interface StudentRepository extends JpaRepository<Student, Integer> {}
