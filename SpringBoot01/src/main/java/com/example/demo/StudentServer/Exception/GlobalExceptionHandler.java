5b9fe150972cd1ac112ce08fc5248852aa09a056
mansvi <itsmansvi@gmail.com>
2026-07-21 11:51:44 +0530

    Global Exception Handler -Runtime Exception(Unchecked), Exception(Checked)

---
+++ SpringBoot01/src/main/java/com/example/demo/StudentServer/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.demo.StudentServer.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeExecutionException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleExecutionException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
